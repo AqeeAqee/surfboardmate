b00a99eee141d032f5b79d7fc7dd2ec17570d56f
AqeeAqee <aqee@msn.com>
2022-10-28 15:05:28 +0800

    add setting menu (with 0% error on change value)

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     if (mode == -1) {
-    	
+        changeSettingIndex(-1)
     }
 })
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -13,8 +13,44 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
         initSettingScene()
     }
 })
+function changeSettingIndex (dir: number) {
+    if (curSettingIndex != -1) {
+        settingSprs[curSettingIndex].setBorder(0, 6)
+    }
+    curSettingIndex += dir
+    if (curSettingIndex < 0) {
+        curSettingIndex = settingNames.length - 1
+    } else if (curSettingIndex > settingNames.length - 1) {
+        curSettingIndex = 0
+    }
+    settingSprs[curSettingIndex].setBorder(1, 6)
+}
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mode == -1) {
+        changeSettingValue(-1)
+    }
+})
+function changeSettingValue (delta: number) {
+    temp = delta + settingValues[curSettingIndex]
+    if (temp < settingMinValues[curSettingIndex]) {
+        temp = settingMaxValues[curSettingIndex]
+    } else if (temp > settingMaxValues[curSettingIndex]) {
+        temp = settingMinValues[curSettingIndex]
+    }
+    settingValues[curSettingIndex] = temp
+    settingSprs[curSettingIndex].setText("" + settingNames[curSettingIndex] + ": " + temp)
+    sdwireless.sdw_mbit_send_value(settingNames[curSettingIndex], temp)
+    pause(11)
+}
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mode == -1) {
+        changeSettingValue(1)
+    }
+})
 function hideSettingScene () {
-	
+    for (let textSpr of settingSprs) {
+        textSpr.setFlag(SpriteFlag.Invisible, true)
+    }
 }
 function drawScanline () {
     t += 2
@@ -36,16 +72,27 @@ function plotBars () {
         picture.fillRect(80, index2 * 40, values[index2], 20, colors[index2])
     }
 }
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mode == -1) {
+        changeSettingIndex(1)
+    }
+})
 sdwireless.sdw_onmbit_string(function (radioMsg) {
     gotData(radioMsg)
 })
 function initSettingScene () {
     if (curSettingIndex == -1) {
-        settingTextSprs = []
-        for (let index = 0; index <= settingNames.length - -1; index++) {
-            settingTextSpr = textsprite.create(settingNames[index])
-            settingTextSprs[index] = settingTextSpr
-            settingTextSpr.setPosition(10, index * 30 + 20)
+        initSettingArrays()
+        for (let index = 0; index <= settingNames.length - 1; index++) {
+            settingSpr = textsprite.create("" + settingNames[index] + ": " + settingValues[index])
+            settingSprs[index] = settingSpr
+            settingSpr.left = 10
+            settingSpr.top = index * 20 + 20
+        }
+        changeSettingIndex(1)
+    } else {
+        for (let textSpr of settingSprs) {
+            textSpr.setFlag(SpriteFlag.Invisible, false)
         }
     }
 }
@@ -66,19 +113,50 @@ function gotData (strData: string) {
         lastTs[index3] = t
     }
 }
+function initSettingArrays () {
+    settingSprs = []
+    settingNames = [
+    "HUE",
+    "b",
+    "c",
+    "d"
+    ]
+    settingValues = [
+    10,
+    1,
+    0,
+    0
+    ]
+    settingMaxValues = [
+    360,
+    4,
+    3,
+    44
+    ]
+    settingMinValues = [
+    1,
+    1,
+    2,
+    3
+    ]
+}
 let strValues: string[] = []
-let settingTextSpr: TextSprite = null
-let settingTextSprs: TextSprite[] = []
+let settingSpr: TextSprite = null
 let lastTs: number[] = []
 let lastValues: number[] = []
 let values: number[] = []
 let t = 0
+let settingMaxValues: number[] = []
+let settingMinValues: number[] = []
+let settingValues: number[] = []
+let temp = 0
 let settingNames: string[] = []
+let settingSprs: TextSprite[] = []
 let curSettingIndex = 0
 let mode = 0
 let colors: number[] = []
 let picture: Image = null
-let sim = true
+let sim = false
 picture = image.create(160, 120)
 scene.setBackgroundImage(picture)
 if (!(sim)) {
@@ -96,8 +174,6 @@ colors = [
 // 1 = bars
 mode = 0
 curSettingIndex = -1
-settingNames = ["a", "b", "c"]
-let settingDefaults = [0, 1, 0]
 game.onUpdateInterval(50, function () {
     if (mode == 0) {
         drawScanline()
